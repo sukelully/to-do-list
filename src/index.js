75c2bc5b385726f89b23b3b4d4ec899c31271451
Luke Sullivan <luke@sukelully.dev>
2025-05-19 13:55:43 +0100

    Dynamic task generation

--- src/index.js
+++ src/index.js
@@ -15,34 +15,55 @@ const taskTest2 = new Task(
     2);
 
 tasksArray.push(taskTest);
-tasksArray.push(taskTest2);
+// tasksArray.push(taskTest2);
 
 const displayTasks = (function () {
-    const projectsContainer = document.getElementById("projects.container");
+    const projectsContainer = document.getElementById("projects-container");
+
     for (const task of tasksArray) {
         const taskDiv = document.createElement("div");
-        taskDiv.classList.add("border", "border-solid", "border-slate-300", "rounded-lg", "p-4", "flex gap-4", "items-start");
-        
+        projectsContainer.appendChild(taskDiv);
+        taskDiv.id = "task-container";
+        taskDiv.classList.add("border", "border-solid", "border-slate-300", "rounded-lg", "p-4", "flex", "gap-4", "items-start");
+
+        const list = document.createElement("ul");
+
         const priorityIcon = document.createElement("i");
         priorityIcon.classList.add("fa-solid", "fa-flag", "py-2");
-
-        const list = document.getElementById("task-test");
+        switch (task.priority) {
+            case 0:
+                break;
+            case 1:
+                priorityIcon.classList.add("text-green-600");
+                break;
+            case 2:
+                priorityIcon.classList.add("text-yellow-600");
+                break;
+            case 3:
+                priorityIcon.classList.add("text-red-600");
+                break;
+        }
+        taskDiv.appendChild(priorityIcon);
 
         Object.entries(task).forEach(([key, value]) => {
             const li = document.createElement("li");
-            li.textContent = `${value}`
             switch (key) {
                 case 'task':
+                    li.textContent = `${value}`
                     li.classList.add('font-bold');
                     break;
                 case 'description':
+                    li.textContent = `${value}`
                     li.classList.add('text-slate-400', 'text-sm');
+                    break;
                 case 'dueDate':
                     li.textContent = `${key === 'dueDate' ? value.toLocaleDateString() : value}`;
+                    break;
                 default:
                     break;
             }
             list.appendChild(li);
+            taskDiv.appendChild(list);
         });
     }
 })();
